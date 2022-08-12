0b02083e1ebf32031621c25f4124a46a9b20df56
TikBai <15710016065@163.com>
2022-08-12 10:57:02 +0800

    增加方法说明

--- UIPasteControl/ViewController.m
+++ UIPasteControl/ViewController.m
@@ -20,13 +20,14 @@
      *iOS 16 中通过 UIPasteboard 获取剪贴板中的内容时，系统会弹出对话框提醒用户是否允许粘贴。如果不想显示该对话框，建议使用新增的 UIPasteControl 控件来读取剪贴板的内容*
      */
     
-    //TestField
+    //初始化TestField
     UITextField *textfield = [[UITextField alloc]initWithFrame:CGRectMake(100, 300, 200, 80)];
     textfield.borderStyle = UITextBorderStyleRoundedRect;
     [self.view addSubview:textfield];
     
     //UIPasteControl 配置
     UIPasteControlConfiguration *pastConfig;
+    /**以下配置未生效,不确定是否为系统问题,或是代码问题!**/
     pastConfig.displayMode = UIPasteControlDisplayModeIconAndLabel;
     pastConfig.cornerStyle = UIButtonConfigurationCornerStyleFixed;
     pastConfig.cornerRadius = 50;
@@ -34,7 +35,7 @@
     pastConfig.baseForegroundColor = [UIColor redColor];
     
 //    UIPasteControl *pastControl = [[UIPasteControl alloc] initWithFrame:CGRectMake(100,200, 200, 150)];
-    //UIPasteControl
+    //初始化UIPasteControl
     UIPasteControl *pastControl = [[UIPasteControl alloc] initWithConfiguration:pastConfig];
     pastControl.frame =CGRectMake(100, 200, 200, 50);
     //关联控件
